42b06f2ba9476e00a8d6d633283f1956a3e7eac5
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-27 11:04:11 +0700

    create order service mock

--- order_service/internal/services/order.go
+++ order_service/internal/services/order.go
@@ -1,5 +1,10 @@
 package services
 
+import (
+	"fmt"
+	"ticket-api/order/internal/dto"
+)
+
 type OrderService struct {
 }
 
@@ -7,6 +12,6 @@ func NewOrderService() *OrderService {
 	return &OrderService{}
 }
 
-func (s *OrderService) CreateOrder() {
-
+func (s *OrderService) CreateOrder(data dto.OrderCreateDto) {
+	fmt.Println("create order with amount: %d, and total amount: %d", data.Amount, data.TotalAnount)
 }
