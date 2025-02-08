3e0df1122f88b3ebf312e705e68efaa8401bab77
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-08 09:17:23 +0700

    new user service

--- user_service/src/app.module.ts
+++ user_service/src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import config from './shared/config';
-
-const ENV = process.env.NODE_ENV || 'local';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `../env/.${ENV}.env`,
-      load: [config],
-    }),
-    UsersModule,
-    EnvironmentConfigModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- user_service/src/shared/config.ts
+++
@@ -1,8 +0,0 @@
-export default () => ({
-  database: {
-    user: process.env.DB_USER || 'admin',
-    password: process.env.DB_PASSWORD || '1234',
-    name: process.env.DB_NAME || 'dbName',
-    port: process.env.DB_PORT || '5432',
-  },
-});
