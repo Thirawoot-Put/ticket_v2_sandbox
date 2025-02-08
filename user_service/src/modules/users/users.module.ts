9c407e93de7d80fe0759dd3e6d1d76f4bd19aae7
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-08 11:21:40 +0700

    user service init

--- user_service/src/modules/users/users.module.ts
+++ user_service/src/modules/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './infrastructure/controllers/users.controller';
+import { UsersService } from './application/services/users.service';
 
 @Module({
   controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
