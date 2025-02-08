f48d7f607bcfa2c47c428a95e337b4fec4ae0e8d
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-08 09:37:08 +0700

    env file

--- user_service/src/app.module.ts
+++ user_service/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV || 'local';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${ENV}.env`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
