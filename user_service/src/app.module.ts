b787f2d6323262326f723e5aeadcba1e954c9fa0
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-10 13:28:15 +0700

    test post register

--- user_service/src/app.module.ts
+++ user_service/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './config/database.config';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 const ENV = process.env.NODE_ENV || 'local';
 
@@ -17,6 +18,7 @@ const ENV = process.env.NODE_ENV || 'local';
     }),
     TypeOrmModule.forRootAsync(databaseConfig),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
