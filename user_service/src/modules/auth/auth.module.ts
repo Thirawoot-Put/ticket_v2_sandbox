3753b5ec4f856cdbe6f965c41d47fcd6fbecc447
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-10 14:23:28 +0700

    health-check

--- user_service/src/modules/auth/auth.module.ts
+++ user_service/src/modules/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './infrastructure/controllers/auth.controller';
 import { AUTH_SERVICE } from './application/ports/in/auth.port-in';
 import { AuthServiceImpl } from './application/services/auth.service';
 import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
